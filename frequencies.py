65deb0a341c5b96eddf33fd25eae601ce6dd506f
MedantSharan <medsharanofficial@gmail.com>
2023-10-09 22:15:45 +0100

    Completed the frequencies method correctly

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies.keys():
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1;
     return frequencies
+
+print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
